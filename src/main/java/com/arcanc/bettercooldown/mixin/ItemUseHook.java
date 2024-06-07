5b1e758105dd5614d57819b00ab810da716c42f4
Merphyx <merphyx@gmail.com>
2024-06-07 23:48:26 +0200

    v1.0.4

--- src/main/java/com/arcanc/bettercooldown/mixin/ItemUseHook.java
+++ src/main/java/com/arcanc/bettercooldown/mixin/ItemUseHook.java
@@ -10,10 +10,12 @@
 package com.arcanc.bettercooldown.mixin;
 
 import com.arcanc.bettercooldown.timer.TimersHandler;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerInteractionManager;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -22,10 +24,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(ClientPlayerInteractionManager.class)
 public class ItemUseHook
 {
+    @Inject(method = "interactBlock", at = @At(value = "RETURN"))
+    public void blockInteractHook(ClientPlayerEntity player, Hand hand, BlockHitResult hitResult, CallbackInfoReturnable<ActionResult> cir) {
+        TimersHandler.handleInteraction(player, hand, true, hitResult.getBlockPos());
+    }
 
     @Inject(method = "interactItem", at = @At(value = "RETURN", ordinal = 1))
-    public void itemUseHook(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir)
-    {
-        TimersHandler.itemUse(player, hand);
+    public void itemUseHook(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
+        TimersHandler.handleInteraction(player, hand, false, null);
     }
 }
